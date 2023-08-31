14fbfe5e05372aebb38d9170de540bbe0560844b
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-08-31 20:51:23 -0300

    Production environment setup

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+  });
 }
 bootstrap();
